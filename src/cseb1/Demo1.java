c40073a20268d4dc4ebc94687847352e3dfd2849
sushma1803 <86114336+sushma1803@users.noreply.github.com>
2022-10-17 16:16:25 +0530

    added print

--- src/cseb1/Demo1.java
+++ src/cseb1/Demo1.java
@@ -5,5 +5,6 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("WELCOME TO CSE");
+     System.out.println("hi");
      }
 }
